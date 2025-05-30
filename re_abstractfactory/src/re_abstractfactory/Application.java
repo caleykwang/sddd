eee4da019d91001fba28e8831da6562401b4ef88
caleykwang <123776456+caleykwang@users.noreply.github.com>
2025-05-30 19:58:06 -0400

    ok

--- re_abstractfactory/src/re_abstractfactory/Application.java
+++ re_abstractfactory/src/re_abstractfactory/Application.java
@@ -25,7 +25,7 @@ public class Application {
         } else {
             throw new IllegalArgumentException("Unknown OS type: " + osType);
         }
-        //poop
+        //poo
         Application app = new Application(factory);
         app.renderUI();
     }
